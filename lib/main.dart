1210daca94932ede3497d023e279eb82536e1fd0
Edward Alexis <edalvb@gmail.com>
2020-12-30 13:03:26 -0500

    Pantalla principal 2 de 4

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "Roboto",
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+          headline4: TextStyle(color: Colors.white),
+          headline6: TextStyle(color: Colors.white),
+        ),
+      ),
       title: "App de Recetas",
       initialRoute: "/",
-      routes: {
-        "/": (context) => HomePage()
-      },
+      routes: {"/": (context) => HomePage()},
     );
   }
 }
--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -31,8 +31,56 @@ class HomePage extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(color: Color(0xFFA1A5AB), fontSize: 20),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 150, top: 30),
+                  child: Text(
+                    "What do you want to cook today?",
+                    style: TextStyle(fontSize: 35),
+                  ),
+                ),
+                SizedBox(height: 45),
+                Row(
+                  children: [
+                    FoodTimeWidget(colors: [Color(0xFFFF114E), Color(0xFFFF6D1B)]),
+                    FoodTimeWidget(colors: [Color(0xFF3023AE), Color(0xFFC86DD7)]),
+                    FoodTimeWidget(colors: [Color(0xFF10A97E), Color(0xFF10A97E)]),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class FoodTimeWidget extends StatelessWidget {
+  List<Color> colors;
+
+  FoodTimeWidget({this.colors});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      width: 175,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: colors),
+        borderRadius: BorderRadius.all(
+          Radius.circular(30),
+        ),
       ),
     );
   }

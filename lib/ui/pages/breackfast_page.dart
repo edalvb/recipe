4708ac065df1d5316204a0ba472d4eee59cc6205
Edward Alexis <edalvb@gmail.com>
2021-02-16 17:06:47 -0500

    Segunda pantalla 3 de 3

--- lib/ui/pages/breackfast_page.dart
+++ lib/ui/pages/breackfast_page.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:recipe/ui/components/border_button.dart';
 
-class BreackfastPage extends StatelessWidget {
+class BreackfastPage extends StatefulWidget {
+  @override
+  _BreackfastPageState createState() => _BreackfastPageState();
+}
+
+class _BreackfastPageState extends State<BreackfastPage> {
+  String searchWord = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,39 +83,125 @@ class BreackfastPage extends StatelessWidget {
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      FavoriteWidget( text: "FRENCH TOAST", assetPath: "assets/images/favorites/favorite1.png"),
-                      FavoriteWidget( text: "OATMEJAL", assetPath: "assets/images/favorites/Bitmap-9.png"),
-                      FavoriteWidget( text: "FRENCH TOAST", assetPath: "assets/images/favorites/Bitmap-4.png"),
-                      FavoriteWidget( text: "FRENCH TOAST", assetPath: "assets/images/favorites/Bitmap-3.png"),
-                      FavoriteWidget( text: "FRENCH TOAST", assetPath: "assets/images/favorites/favorite1.png"),
+                      FavoriteWidget(
+                        text: "FRENCH TOAST",
+                        assetPath: "assets/images/favorites/favorite1.png",
+                        onTap: () {
+                          print("hola");
+                        },
+                      ),
+                      FavoriteWidget(
+                          text: "OATMEJAL",
+                          assetPath: "assets/images/favorites/Bitmap-9.png"),
+                      FavoriteWidget(
+                          text: "FRENCH TOAST",
+                          assetPath: "assets/images/favorites/Bitmap-4.png"),
+                      FavoriteWidget(
+                          text: "FRENCH TOAST",
+                          assetPath: "assets/images/favorites/Bitmap-3.png"),
+                      FavoriteWidget(
+                          text: "FRENCH TOAST",
+                          assetPath: "assets/images/favorites/favorite1.png"),
                     ],
                   ),
+                ),
+                SizedBox(height: 30),
+                GridView.count(
+                  crossAxisCount: 3,
+                  shrinkWrap: true,
+                  childAspectRatio: 9 / 16,
+                  physics: ClampingScrollPhysics(),
+                  children: [
+                    FavoriteWidget(
+                      text: "FRUIT SALAD",
+                      assetPath: "assets/images/favorites/Bitmap-1.png",
+                      onTap: () {
+                        print("hola");
+                      },
+                      height: 150,
+                      width: 125,
+                    ),
+                    FavoriteWidget(
+                      text: "MOROCCAN CHICKEN SKILLET",
+                      assetPath: "assets/images/favorites/Bitmap-2.png",
+                      height: 150,
+                      width: 125,
+                    ),
+                    FavoriteWidget(
+                      text: "THAI TOM YUM SOUP",
+                      assetPath: "assets/images/favorites/Bitmap-10.png",
+                      height: 150,
+                      width: 125,
+                    ),
+                    FavoriteWidget(
+                      text: "FRUIT SALAD",
+                      assetPath: "assets/images/favorites/Bitmap-1.png",
+                      onTap: () {
+                        print("hola");
+                      },
+                      height: 150,
+                      width: 125,
+                    ),
+                    FavoriteWidget(
+                      text: "MOROCCAN CHICKEN SKILLET",
+                      assetPath: "assets/images/favorites/Bitmap-2.png",
+                      height: 150,
+                      width: 125,
+                    ),
+                    FavoriteWidget(
+                      text: "THAI TOM YUM SOUP",
+                      assetPath: "assets/images/favorites/Bitmap-10.png",
+                      height: 150,
+                      width: 125,
+                    ),
+                  ],
                 )
               ],
             ),
-          )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: MyButtomSheet(
+              defaultValue: this.searchWord,
+              searchCallBack: _requestSearch,
+            ),
+          ),
         ],
       ),
     );
   }
+
+  _requestSearch(String word) {
+    print(word);
+  }
 }
 
 class FavoriteWidget extends StatelessWidget {
-  String assetPath;
-  String text;
+  final String assetPath;
+  final String text;
+  final VoidCallback onTap;
+  final double height;
+  final double width;
 
-  FavoriteWidget({this.assetPath, this.text});
+  FavoriteWidget({
+    this.assetPath,
+    this.text,
+    this.onTap,
+    this.height = 125,
+    this.width = 110,
+  });
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: this.onTap,
       child: SizedBox(
-        width: 110,
+        width: this.width,
         child: Column(
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(8),
-              child: Image.asset(assetPath, height: 120),
+              child: Image.asset(assetPath, height: this.height),
             ),
             SizedBox(height: 5),
             Text(
@@ -121,5 +213,49 @@ class FavoriteWidget extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+typedef SearchCallBack = Function(String word);
+
+class MyButtomSheet extends StatelessWidget {
+  TextEditingController searchController;
+  final SearchCallBack searchCallBack;
+
+  MyButtomSheet({String defaultValue = "", this.searchCallBack}) {
+    searchController = TextEditingController(text: defaultValue);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: this.searchController,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                    hintText: "Search your recipe",
+                    hintStyle: TextStyle(color: Colors.white70)),
+              ),
+            ),
+            IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                onPressed: OnSearchPressed)
+          ],
+        ),
+      ),
+    );
+  }
+
+  void OnSearchPressed() {
+    this.searchCallBack(this.searchController.text);
   }
 }

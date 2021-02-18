5e19ce92032528a9401dddab2df8d99e1e236d99
Edward Alexis <edalvb@gmail.com>
2021-02-18 15:30:11 -0500

    Pantalla de más detalles 2 de 3

--- lib/ui/pages/dish_details_more_page.dart
+++ lib/ui/pages/dish_details_more_page.dart
@@ -59,6 +59,12 @@ class DishDetailsMorePage extends StatelessWidget {
                   ],
                 ),
               ),
+              StepWidget(
+                text:
+                    "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.",
+                number: 1,
+                heigth: 100,
+              )
             ],
           ),
         ),
@@ -95,4 +101,97 @@ class PlayWidget extends StatelessWidget {
       ),
     );
   }
+}
+
+class StepWidget extends StatelessWidget {
+  int number;
+  String text;
+  int heigth;
+  StepWidget({this.number, this.text, this.heigth});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    color: Color(0xffFF4432),
+                    borderRadius: BorderRadius.circular(7)),
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  getFormatedNumber(),
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+              CustomPaint(
+                painter: LineDashedPainter(heigth),
+              )
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "STEP",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(text)
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String getFormatedNumber() {
+    if (number > 9) {
+      return number.toString();
+    } else {
+      return "0$number";
+    }
+  }
+}
+
+class LineDashedPainter extends CustomPainter {
+  final int heigth;
+
+  LineDashedPainter(this.heigth);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()..strokeWidth = 1;
+    paint.color = Colors.grey;
+    var max = heigth;
+    var dashWidth = 3;
+    var dashSpace = 3;
+    double startY = 0;
+
+    while (max >= 0) {
+      canvas.drawLine(Offset(0, startY), Offset(0, startY + dashWidth), paint);
+      final space = (dashSpace + dashWidth);
+      startY += space;
+      max -= space;
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
